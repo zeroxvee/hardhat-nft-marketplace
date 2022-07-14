c6e0840e9f21e98446c93806c9783a5975c17a1b
vbazhutin <vladimir.bazhutin@gmail.com>
2022-07-14 13:35:08 -0400

    NftMarketplace: deploy script

---
+++ deploy/01-deploy-nft-marketplace.js
@@ -0,0 +1,23 @@
+const { getNamedAccounts, deployments, network } = require("hardhat")
+const { verify } = require("../utils/verify")
+
+module.exports = async function () {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const args = []
+
+    const nftMarketPlace = await deploy("NftMarketplace", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (network.chainId == 31337 && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying")
+        await verify(nftMarketPlace.address, args)
+    }
+    log("--------------------------")
+}
+
+module.exports.tags = ["all", "nftMarketPlace"]
