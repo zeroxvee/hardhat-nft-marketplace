aa4db152cc1c74dc2336c1a38fcb98558486ddbd
vbazhutin <vladimir.bazhutin@gmail.com>
2022-07-21 13:57:54 -0400

    Added functionality for ABI update

--- deploy/99-update-front-end.js
+++ deploy/99-update-front-end.js
@@ -2,12 +2,28 @@ const { ethers, network } = require("hardhat")
 const fs = require("fs")
 
 const frontEndContractsFile = "../nextjs-nft-marketplace/constants/networkMapping.json"
+const frontEndAbiLocation = "../nextjs-nft-marketplace/constants/"
 
 module.exports = async function () {
     if (process.env.UPDATE_FRONT_END) {
         console.log("Updating front end...")
         await updateContractAddresses()
+        await updateAbi()
     }
+}
+
+async function updateAbi() {
+    const nftMarketPlace = await ethers.getContract("NftMarketplace")
+    fs.writeFileSync(
+        `${frontEndAbiLocation}NftMarketplace.json`,
+        nftMarketPlace.interface.format(ethers.utils.FormatTypes.json)
+    )
+
+    const basicNft = await ethers.getContract("BasicNft")
+    fs.writeFileSync(
+        `${frontEndAbiLocation}BasicNft.json`,
+        basicNft.interface.format(ethers.utils.FormatTypes.json)
+    )
 }
 
 async function updateContractAddresses() {
